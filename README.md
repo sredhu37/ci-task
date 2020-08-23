34e628977c443fd61320f33ccf75f5fabdac4d7e
Sunny Redhu <redhu.sunny1994@gmail.com>
2020-08-23 13:02:03 +0530

    Initial setup (#1)

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ci-task
+
+[![Build Status](http://www.abcdpractice.com:8080/buildStatus/icon?job=ci-task-main%2Fmaster)](http://www.abcdpractice.com:8080/job/ci-task-main/job/master/)
---
+++ ci-cd/pipelines/utils.groovy
@@ -0,0 +1,40 @@
+owner = "sredhu37"
+repo = "ci-task"
+commitHash = ""
+
+def setGHStatusCheck(String checkName, String status, String description="No description available") {
+    httpRequest (
+        acceptType: 'APPLICATION_JSON',
+        consoleLogResponseBody: true,
+        authentication: 'sunny-github-credentials',
+        contentType: 'APPLICATION_JSON',
+        httpMode: 'POST',
+        requestBody: """{
+            "context": "${checkName}",
+            "state": "${status}",
+            "target_url": "${env.BUILD_URL}",
+            "description": "${description}"
+        }""",
+        responseHandle: 'NONE',
+        timeout: 10,
+        url: "https://api.github.com/repos/${owner}/${repo}/statuses/${commitHash}"
+    )
+}
+
+def stageWithGHStatusCheck(String stageName, Closure closureFunction) {
+    String stageStatus = 'pending'
+
+    try {
+        stage(stageName) {
+            setGHStatusCheck(stageName, stageStatus, "Execution has started...")
+            closureFunction()
+        }
+        stageStatus = 'success'
+    } catch(exc) {
+        stageStatus = 'failure'
+    } finally {
+        setGHStatusCheck(stageName, stageStatus)
+    }
+}
+
+return this
