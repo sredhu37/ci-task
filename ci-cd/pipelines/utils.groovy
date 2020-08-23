ff283a143aa8267b6fa5dcfc8a914dd4945da36d
i342082 <sunny.redhu@sap.com>
2020-08-23 12:46:40 +0530

    status checks

--- ci-cd/pipelines/utils.groovy
+++ ci-cd/pipelines/utils.groovy
@@ -1,5 +1,23 @@
-def setGHStatusCheck(String checkName, String status, String description="No description available") {
+owner = "sredhu37"
+repo = "ci-task"
+commitHash = ""
 
+def setGHStatusCheck(String checkName, String status, String description="No description available") {
+    httpRequest (
+        acceptType: 'APPLICATION_JSON',
+        consoleLogResponseBody: true,
+        contentType: 'APPLICATION_JSON',
+        httpMode: 'POST',
+        requestBody: """{
+            "context": "${checkName}",
+            "state": "${status}",
+            "target_url": "${env.BUILD_URL}",
+            "description": "${description}"
+        }""",
+        responseHandle: 'NONE',
+        timeout: 10,
+        url: "https://api.github.com/repos/${owner}/${repo}/statuses/${commitHash}"
+    )
 }
 
 def stageWithGHStatusCheck(String stageName, Closure closureFunction) {
